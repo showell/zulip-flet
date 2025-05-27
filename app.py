61c47c794cc7abfb0e7cf85baf307ba6c27b92c1
Steve Howell <showell30@yahoo.com>
2025-05-27 09:17:46 -0400

    buddy

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import asyncio
-
 import flet as ft
 from data_layer import get_database
 
@@ -7,8 +5,6 @@ async def main(page: ft.Page):
     page.title = "Flet counter example"
     page.vertical_alignment = "center"
     page.horizontal_alignment = "center"
-    page.window.height = 300
-    page.window.width = 500
 
     text = ft.Text(
         value="Loading",
@@ -20,20 +16,26 @@ async def main(page: ft.Page):
 
     database = await get_database()
 
+    buddy_list_container = await get_buddy_list_container(database)
+    page.controls = [buddy_list_container]
+    page.update()
+
+
+async def get_buddy_list_container(database):
     items = []
     for user in sorted(database.user_dict.values(), key=lambda u: u.name):
-        items.append(ft.Text(user.name, color=ft.Colors.BLACK))
+        items.append(ft.Text(user.name, color=ft.Colors.BLACK, size=17))
 
     buddy_list = ft.ListView(items)
     buddy_list_container = ft.Container(
         buddy_list,
-        border =ft.border.all(1, color=ft.Colors.BLUE),
+        border=ft.border.all(1, color=ft.Colors.BLUE),
         bgcolor=ft.Colors.LIGHT_BLUE_50,
         width=200,
         alignment=ft.alignment.top_right,
         expand=True
     )
-    page.controls = [buddy_list_container]
-    page.update()
+    return buddy_list_container
+
 
 ft.app(main)
