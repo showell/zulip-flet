2fb7123c6240283d7246c37845270453b6c7ee39
Steve Howell <showell30@yahoo.com>
2025-05-27 13:43:54 -0400

    progress

--- message_pane.py
+++ message_pane.py
@@ -7,34 +7,43 @@ async def message_row(database, message):
     item = ft.Row(
         controls=[
             ft.Image(src=sender.avatar_url, height=30),
-            ft.Text(
-                message.content,
-                color=ft.Colors.BLACK,
-                bgcolor=ft.Colors.GREY_200,
-                size=13,
-                overflow=ft.TextOverflow.ELLIPSIS,
-                selectable=True,
-                expand=True,
-            ),
+            ft.Column(
+                controls=[
+                    ft.Text(sender.name, size=14),
+                    ft.Text(
+                        message.content,
+                        color=ft.Colors.BLACK,
+                        bgcolor=ft.Colors.GREY_200,
+                        size=13,
+                        overflow=ft.TextOverflow.ELLIPSIS,
+                        selectable=True,
+                        expand=True,
+                    ),
+                    ft.Text("need emojis still", size=3, text_align=ft.TextAlign.RIGHT),
+                ]
+            )
         ],
+        vertical_alignment=ft.CrossAxisAlignment.START,
+        expand=True,
     )
 
-    def press(_):
-        print("pressed")
-
-    return ft.Container(item, padding=4, on_long_press=press)
-
+    return ft.Container(
+        item,
+        border=ft.border.all(1, ft.Colors.RED_100),
+        padding=5,
+        expand=True,
+    )
 
 async def make_message_pane(database):
-    items = []
-    for user in sorted(database.message_dict.values(), key=lambda u: u.timestamp):
-        items.append(await message_row(database, user))
+        items = []
+        for user in sorted(database.message_dict.values(), key=lambda u: u.timestamp):
+            items.append(await message_row(database, user))
 
-    list_view = ft.ListView(items)
-    list_container = ft.Container(
-        list_view,
-        bgcolor=ft.Colors.GREY_200,
-        width=700,
-        padding=10,
-    )
-    return list_container
+        list_view = ft.ListView(items)
+        list_container = ft.Container(
+            list_view,
+            bgcolor=ft.Colors.GREY_200,
+            width=700,
+            padding=10,
+        )
+        return list_container
