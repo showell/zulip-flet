a2c5f436cd3e46e69b00336427e01ec2f9cec188
Steve Howell <showell30@yahoo.com>
2025-05-27 07:55:18 -0400

    serialize

--- data_layer.py
+++ data_layer.py
@@ -64,11 +64,7 @@ async def populate_database(zulip_api, register_info):
     database.populate_users(register_info.realm_users)
     database.populate_streams(register_info.streams)
 
-    db_json = database.model_dump_json()
-    database = Database.model_validate_json(db_json)
-    print("\n\n---------\n\n")
-    print(database)
-    print(database.user_dict)
+    return database
 
 
 async def register(zulip_api):
@@ -83,8 +79,25 @@ async def register(zulip_api):
         print("queue_id:", register_info.queue_id)
         return register_info
 
-
 async def main():
+    zulip_api = ZulipApi(HOST, USER_NAME, API_KEY)
+    register_info = await register(zulip_api)
+
+    database = await populate_database(zulip_api, register_info)
+
+    db_json = database.model_dump_json()
+    print(type(db_json))
+    fn = "database.json"
+    with open(fn, "w", encoding="utf8") as database_file:
+        database_file.write(db_json)
+    print(f"Database saved to {fn}")
+
+    with open(fn, encoding="utf8") as database_file:
+        db_json = database_file.read()
+    database = Database.model_validate_json(db_json)
+    print([user.name for user in database.user_dict.values()])
+
+async def original_main():
     zulip_api = ZulipApi(HOST, USER_NAME, API_KEY)
     register_info = await register(zulip_api)
 
@@ -97,5 +110,5 @@ async def main():
 
     await process_events(zulip_api, event_info)
 
-
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
