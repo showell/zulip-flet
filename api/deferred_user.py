93da6369a80ec4082c27012d457af34dd22eabc9
Steve Howell <showell30@yahoo.com>
2025-05-30 07:56:18 -0400

    comments

--- api/deferred_user.py
+++ api/deferred_user.py
@@ -3,6 +3,67 @@ from typing import Awaitable, Callable
 
 from user import User
 
+"""
+When we render a bunch of rows that have user data in them, it can
+be a bit complicated to deal with the fact that some users may not
+yet have been downloaded to the client.
+
+For example, you might be rendering a bunch of Zulip messages, but
+when you start the process, your message objects may only have
+sender_id, instead of the hydrated User object for sender.  Now,
+usually, most of our User data is gonna be local, so it's mostly
+a matter of calling something like `get_local_user` to hydrate the
+user in this case (i.e. message.sender_id -> message.sender).
+
+But because we can't rely on everything being local, we want a
+more disciplined approach that handles the async stuff correctly.
+
+Moreover, we want to ensure we don't make O(N) calls to the server,
+and we certainly want to avoid even a single call to the server
+if our local cache of users is sufficient for the request.
+
+The solution here is to use a DeferredUser object in our messages.
+
+Here is example code to populate the messages...
+
+First, we change our message object class to use
+`deferred_sender: DeferredUser` instead of `sender: User`:
+
+    @dataclass
+    class HydratedMessage:
+        deferred_sender: DeferredUser # <======== NOTICE THIS!
+        content: str
+        timestamp: int
+    
+        @staticmethod
+        def create(*, message: Message, factory: DeferredUserFactory) -> "HydratedMessage":
+            return HydratedMessage(
+                deferred_sender=factory.create_user(message.sender_id), # <==== Synchronous!
+                content=message.content,
+                timestamp=message.timestamp,
+            )
+        
+Then we change our controller code to look like below.  Note that we create
+our messages synchronously, and its only the call to `factory.finalize` that
+has an `await` before it.
+
+        hydrated_messages = [
+            HydratedMessage.create(message=m, factory=factory) for m in messages
+        ]
+        helper = DeferredUserHelper(
+            maybe_get_local_user=self.maybe_get_local_user,
+            get_remote_users=self.get_remote_users,
+        )
+        await factory.finalize(helper=helper)
+        return sorted(hydrated_messages, key=lambda m: m.timestamp)
+        
+And then we actually go to render the messages (after the `await`), we
+simply do this in our render code:
+
+    sender = hydrated_message.deferred_sender.full_object(
+    # and then use sender.full_name, sender.avatar_url, etc.
+"""
+
 
 @dataclass
 class DeferredUserHelper:
