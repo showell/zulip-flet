1df21a3f9f07073b63a7537a2233a76005e8121d
Steve Howell <showell30@yahoo.com>
2025-06-03 12:50:07 -0400

    address

--- api/address.py
+++ api/address.py
@@ -1,6 +1,13 @@
 from pydantic import BaseModel
+from topic_table import TopicTable
 
 
 class Address(BaseModel):
     type: str
     topic_id: int
+
+    def name(self, *, topic_table: TopicTable) -> str:
+        if self.type == "stream":
+            return topic_table.get_topic(self.topic_id).name
+        else:
+            return ""
--- api/hydrated_message.py
+++ api/hydrated_message.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from address import Address
 from database import Database
 from deferred_user import DeferredUser, DeferredUserFactory
 from message import Message
@@ -9,20 +10,17 @@ class HydratedMessage:
     deferred_sender: DeferredUser
     content: str
     timestamp: int
+    address: Address
     address_name: str
 
     @staticmethod
     def create(
         *, message: Message, factory: DeferredUserFactory, database: Database
     ) -> "HydratedMessage":
-        if message.address.type == "stream":
-            address_name = database.topic_table.get_topic(message.address.topic_id).name
-        else:
-            address_name = ""
-
         return HydratedMessage(
             deferred_sender=factory.create_user(message.sender_id),
             content=message.content,
             timestamp=message.timestamp,
-            address_name=address_name,
+            address=message.address,
+            address_name=message.address.name(topic_table=database.topic_table),
         )
