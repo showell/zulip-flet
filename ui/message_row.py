7cce5624f0f7949fe23e2ad07fadd1bf4067532c
Steve Howell <showell30@yahoo.com>
2025-06-03 18:53:28 -0400

    link

--- ui/message_row.py
+++ ui/message_row.py
@@ -1,9 +1,19 @@
 import flet as ft
 
 
+class AddressLink:
+    def __init__(self, hydrated_message):
+        self.control = ft.Text(
+            hydrated_message.address_name,
+            size=10,
+            weight=ft.FontWeight.BOLD,
+        )
+
+
 class MessageRow:
     def populate(self, hydrated_message, *, width):
         sender = hydrated_message.deferred_sender.full_object()
+        address_link = AddressLink(hydrated_message)
 
         item = ft.Row(
             controls=[
@@ -13,11 +23,7 @@ class MessageRow:
                         ft.Row(
                             controls=[
                                 ft.Text(sender.name, size=14),
-                                ft.Text(
-                                    hydrated_message.address_name,
-                                    size=12,
-                                    weight=ft.FontWeight.BOLD,
-                                ),
+                                address_link.control,
                             ],
                         ),
                         ft.Markdown(
