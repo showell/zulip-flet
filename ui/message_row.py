02e15d6c253c5d33a59ebf22c42f2c61b20fb816
Steve Howell <showell30@yahoo.com>
2025-06-03 18:56:41 -0400

    click

--- ui/message_row.py
+++ ui/message_row.py
@@ -3,11 +3,18 @@ import flet as ft
 
 class AddressLink:
     def __init__(self, hydrated_message):
-        self.control = ft.Text(
+        text = ft.Text(
             hydrated_message.address_name,
             size=10,
             weight=ft.FontWeight.BOLD,
         )
+
+        self.control = ft.Container(text)
+
+        def on_click(_):
+            print(hydrated_message.address)
+
+        self.control.on_click = on_click
 
 
 class MessageRow:
