38ee1ceba2105297ea86e410cb79bfc2c2af636e
Steve Howell <showell30@yahoo.com>
2025-06-17 10:38:05 -0400

    paragraph

--- ui/message_parser.py
+++ ui/message_parser.py
@@ -1,8 +1,10 @@
 from lxml import etree
 from pydantic import BaseModel
 
+
 class BaseNode(BaseModel):
     pass
+
 
 class DumbNode(BaseNode):
     text: str
@@ -11,12 +13,24 @@ class DumbNode(BaseNode):
     def as_text(self):
         return self.text + "".join(c.as_text() for c in self.children)
 
+
+class P_Node(BaseNode):
+    text: str
+    children: list[BaseNode]
+
+    def as_text(self):
+        return self.text + "".join(c.as_text() for c in self.children) + "\n\n"
+
+
 def get_node(elem):
     text = elem.text or ""
     children = [get_node(c) for c in elem]
-    return DumbNode(text=text, children=children)
+    if elem.tag == "p":
+        return P_Node(text=text, children=children)
+    else:
+        return DumbNode(text=text, children=children)
 
 
 def text_content(html):
     root = etree.HTML("<html>" + html + "</html>")
-    return "raw: " + get_node(root).as_text()
+    return get_node(root).as_text()
