712951c0c967a454102a38f00de79e9afc1493c6
Steve Howell <showell30@yahoo.com>
2025-06-17 09:58:09 -0400

    basic recursion

--- ui/message_parser.py
+++ ui/message_parser.py
@@ -1,6 +1,11 @@
 from lxml import etree
 
+def get_text(elem):
+    text = elem.text or ""
+    text += "".join(get_text(c) for c in elem)
+    return text
+
 def text_content(html):
     root = etree.HTML("<html>" + html + "</html>")
-    return "raw html: " + etree.tostring(root).decode("utf8")
+    return get_text(root)
 
