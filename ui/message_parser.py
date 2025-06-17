6d802f6ea7f96a32896ec8d1d2a36ce596b8d5f0
Steve Howell <showell30@yahoo.com>
2025-06-17 11:11:00 -0400

    body

--- ui/message_parser.py
+++ ui/message_parser.py
@@ -2,19 +2,33 @@ from lxml import etree
 from pydantic import BaseModel
 
 
+def enclose(tag, inners):
+    return f"<{tag}>{inners}</{tag}>"
+
+
 class BaseNode(BaseModel):
     pass
 
 
 class DumbNode(BaseNode):
+    tag: str
     text: str
     children: list[BaseNode]
 
     def as_text(self):
-        return self.text + "".join(c.as_text() for c in self.children)
+        return enclose(
+            self.tag, self.text + "".join(c.as_text() for c in self.children)
+        )
 
 
-class P_Node(BaseNode):
+class BodyNode(BaseNode):
+    children: list[BaseNode]
+
+    def as_text(self):
+        return "".join(c.as_text() for c in self.children)
+
+
+class PNode(BaseNode):
     text: str
     children: list[BaseNode]
 
@@ -25,12 +39,16 @@ class P_Node(BaseNode):
 def get_node(elem):
     text = elem.text or ""
     children = [get_node(c) for c in elem]
-    if elem.tag == "p":
-        return P_Node(text=text, children=children)
+    if elem.tag == "html":
+        return get_node(elem[0])
+    elif elem.tag == "body":
+        return BodyNode(children=children)
+    elif elem.tag == "p":
+        return PNode(text=text, children=children)
     else:
-        return DumbNode(text=text, children=children)
+        return DumbNode(tag=elem.tag, text=text, children=children)
 
 
 def text_content(html):
-    root = etree.HTML("<html>" + html + "</html>")
+    root = etree.HTML("<body>" + html + "</body>")
     return get_node(root).as_text()
